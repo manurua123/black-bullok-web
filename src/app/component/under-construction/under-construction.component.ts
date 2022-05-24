2100c0f65efccdc7728425d8f41a5e06d5bb4c43
Manuel Rua <manu.rua.123@gmail.com>
2022-05-24 20:59:27 -0300

    .

--- src/app/component/under-construction/under-construction.component.ts
+++ src/app/component/under-construction/under-construction.component.ts
@@ -15,6 +15,7 @@ export class UnderConstructionComponent implements OnInit {
     hours: number;
     minutes: number;
     seconds: number;
+
   };
   @Input() finishDateString: string = '';
   finishDate: Date = new Date();
@@ -29,10 +30,10 @@ export class UnderConstructionComponent implements OnInit {
 
     // Inicializamos el momento que falta hasta llegaral tiempo objetivo con valores en 0
     this.time = {
-      days: 0, hours: 0, minutes: 0, seconds: 0
+      days: 0, hours: 0, minutes: 0, seconds: 0,
     };
     // Creamos la fecha a partir de la fecha en formato string AAAA-MM-dd HH:mm:ss
-    this.finishDate = new Date('January 10 2022 00:00:1');
+    this.finishDate = new Date('July 10 2022 00:00:1');
 
     this.start().subscribe();
   }
@@ -41,18 +42,19 @@ export class UnderConstructionComponent implements OnInit {
     const now = new Date();
     const diff = this.finishDate.getTime() - now.getTime();
 
-
     // Cálculos para sacar lo que resta hasta ese tiempo objetivo / final
     const days = Math.floor(diff / (1000 * 60 * 60 * 24));
     const hours = Math.floor(diff / (1000 * 60 * 60));
     const mins = Math.floor(diff / (1000 * 60));
     const secs = Math.floor(diff / 1000);
 
+
     // La diferencia que se asignará para mostrarlo en la pantalla
     this.time.days = days;
     this.time.hours = hours - days * 24;
     this.time.minutes = mins - hours * 60;
     this.time.seconds = secs - mins * 60;
+
   }
 
   start() {
